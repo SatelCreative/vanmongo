82c648966e5a2ad29cee79bfcc79c66084fdab19
Quinn Blenkinsop <quinn@blenkinsop.net>
2021-06-08 12:05:12 -0700

    Set up mechanism to create indexes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,10 @@
+from importlib import reload
+
 import pytest
 from pydantic import BaseModel
 from pymongo import MongoClient
+
+from satel_mongo import Client
 
 
 class TestConfig(BaseModel):
@@ -18,6 +22,12 @@ def mongo(test_config):
     return MongoClient(test_config.mongo_url)
 
 
+@pytest.fixture(scope="session")
+def db(mongo, test_config):
+    return mongo[test_config.mongo_database]
+
+
 @pytest.fixture(autouse=True)
-def reset(mongo, test_config):
+async def reset(mongo, test_config):
+    await Client.shutdown()
     mongo.drop_database(test_config.mongo_database)
--- tests/test_client.py
+++ tests/test_client.py
@@ -8,12 +8,11 @@ class Context(BaseModel):
     admin: bool
 
 
-class Item(BaseDocument):
-    pass
-
-
 @pytest.mark.asyncio
 async def test_client(test_config):
+    class Item(BaseDocument):
+        pass
+
     await Client.initialize(
         mongo_url=test_config.mongo_url, mongo_database=test_config.mongo_database
     )
@@ -29,3 +28,26 @@ async def test_client(test_config):
     find_result = await items.find_one_by_id(create_result.id)
 
     assert find_result == create_result
+
+
+def assert_indexes(collection, **kwargs):
+    info = collection.index_information()
+    info.pop("_id_")  # Default _id index
+
+    indexes = {}
+    for name, value in info.items():
+        indexes[name] = value["key"]
+
+    assert indexes == kwargs
+
+
+@pytest.mark.asyncio
+async def test_indexes(db, test_config):
+    class Item(BaseDocument):
+        pass
+
+    await Client.initialize(
+        mongo_url=test_config.mongo_url, mongo_database=test_config.mongo_database
+    )
+
+    assert_indexes(db[Item.collection], id=[("id", 1)])
