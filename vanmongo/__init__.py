58d5a5224e925860cc3b9fc3ea16fcec98af88ba
Frank Chung <ponty33@gmail.com>
2021-06-23 16:14:11 -0700

    add version in init for flit

--- vanmongo/__init__.py
+++ vanmongo/__init__.py
@@ -1,3 +1,7 @@
+"""A library to handle MongoDB Operations"""
+
+__version__ = '0.1.0'
+
 from .connection import Connection, Edge, PageInfo
 from .events import EventType
 from .main import BaseCollection, BaseDocument, Client
