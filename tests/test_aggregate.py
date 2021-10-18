ebd0efc691d11b4f5c2599ca2c3bfe47bd9dd5de
Frank Chung <ponty33@gmail.com>
2021-10-18 11:34:22 -0700

    add test

---
+++ tests/test_aggregate.py
@@ -0,0 +1,44 @@
+from typing import List
+
+import pytest
+
+from vanmongo import BaseDocument, Client
+
+
+@pytest.mark.asyncio
+async def test_aggregate(test_config):
+    sort_options = ["index"]
+
+    class Item(BaseDocument, sort_options=sort_options):
+        index: int
+
+    await Client.initialize(
+        mongo_url=test_config.mongo_url, mongo_database=test_config.mongo_database
+    )
+
+    client = Client()
+
+    items = client.use(Item)
+
+    fixture: List[Item] = []
+    fixture_raw: List[dict] = []
+    for index in range(50):
+        item = await items.create_one({"index": index})
+        fixture.append(item)
+        fixture_raw.append(item.dict())
+
+    aggregation_pipeline = [
+        {"$match": {"index": {"$exists": True}}},
+        {"$sort": {"created_at": 1}},
+    ]
+    sort_by_creation = [
+        item
+        async for item in items.aggregate(aggregation_pipeline=aggregation_pipeline)
+    ]
+    assert sort_by_creation == fixture
+
+    sort_by_creation_raw = [
+        item
+        async for item in items.aggregate_raw(aggregation_pipeline=aggregation_pipeline)
+    ]
+    assert sort_by_creation_raw == fixture_raw
